3a112a8ff65359d580523d2836a7515cba8e12a9
Fregoss <o0frego0o@hotmail.com>
2018-02-09 19:18:44 +0200

    refactored

---
+++ atm solution/atm_refactored.py
@@ -0,0 +1,25 @@
+# declaring vars
+balance = 500
+validVlaues = [100,50,10,5,2,1]
+
+def withdraw (balance, request) :
+    currentBalance = balance - request
+    print ("Current balance =",balance)
+    
+    if request > balance :
+        print ("Can't give you all this money !!")
+        return
+       
+    #iterating over given vals
+    for index in validVlaues :
+        # iterating over every val until not valid
+        while request - index >= 0 :
+                print ("give " + str(index))
+                request -= index # updatng request value
+    return currentBalance
+
+
+balance = withdraw(balance, 277)
+balance = withdraw(balance, 30)
+balance = withdraw(balance, 5)
+balance = withdraw(balance, 500)
