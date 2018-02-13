5fdd709eda4ee9e0742b33efbddf1256b5883796
Unknown <o0frego0o@hotmail.com>
2018-02-13 16:40:37 +0200

    entities

---
+++ Forums/main.py
@@ -0,0 +1,8 @@
+import models
+
+member1 = models.Member("member1", 3)
+member2 = models.Member("member2", 3.5)
+
+post1 = models.Post("title1","subject1")
+post2 = models.Post("title2","subject2")
+post3 = models.Post("title3","subject3")
