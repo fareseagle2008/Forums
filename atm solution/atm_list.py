59d344f3393d6bb7a64f22b2f28701814bb3a3bb
Unknown <o0frego0o@hotmail.com>
2018-02-13 15:00:42 +0200

    lists solution

---
+++ atm solution/atm_list.py
@@ -0,0 +1,51 @@
+class ATM:
+
+    def __init__(self, balance, bank_name):
+        #self.balance = balance
+        self.bank_name = bank_name
+        self.balance_history = [balance]
+
+    def withdraw(self, request):
+
+        valid_values = [100, 50, 10, 5, 2, 1]
+        current_balance = self.balance_history[len(self.balance_history)-1]-request
+
+        print("Welcome to",self.bank_name)
+        print("Current balance =", self.balance_history[-1])
+        print("==================================")
+
+        # edge cases tests
+        if request > self.balance_history[-1]:
+            print("Can't give you all this money !!")
+        elif request <= 0:
+            print('invalid request')
+        else:
+            self.balance_history.append(self.balance_history[-1]-request)
+            #print("==================================\n",self.balance_history,"\n==================================")
+            # iterating over given values
+            for index in valid_values:
+                # iterating over every val until not valid
+                while request - index >= 0:
+                    print("give " + str(index))
+                    request -= index  # updating request value
+        print("==================================")
+        return current_balance
+
+    def show_withdrawals(self):
+        for withdrawal in self.balance_history:
+            print(withdrawal)
+
+balance1 = 500
+balance2 = 1000
+
+atm1 = ATM(balance1, "Smart Bank")
+atm2 = ATM(balance2, "Baraka Bank")
+
+atm1.withdraw(277)
+atm1.withdraw(800)
+
+atm2.withdraw(100)
+atm2.withdraw(2000)
+
+atm1.show_withdrawals()
+atm2.show_withdrawals()
